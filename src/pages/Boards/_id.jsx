e7cacfce6b9ae50be229275dafba24d6c4ce0342
caloc <quangdv2802@gmail.com>
2024-09-03 19:44:30 +0700

    less64: add column and card

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,6 +4,7 @@ import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { useState, useEffect } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
+import { mockData } from '~/apis/mock-data'
 
 function Board() {
   const [board, setBoard] = useState(null)
@@ -23,7 +24,7 @@ function Board() {
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
       <BoardBar board={board} />
-      <BoardContent board={board} />
+      <BoardContent board={mockData.board} />
     </Container>
   )
 }
